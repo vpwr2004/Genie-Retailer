e2b983e5e3e1cba80bfa0f81b8e63bc090384f0b
chandresh-t75 <2100430100016@bietjhs.ac.in>
2024-06-23 18:01:27 +0530

    "new changes done "

--- App/notification/notificationServices.js
+++ App/notification/notificationServices.js
@@ -92,33 +92,34 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { setNewRequests, setOngoingRequests, setRetailerHistory } from '../redux/reducers/requestDataSlice';
 // import notifee from "@notifee/react-native"
 
+// "@expo/config-plugins": "^7.8.0",
+// "@expo/prebuild-config": "^6.7.0",
 
+// async function onDisplayNotification() {
+//     // Request permissions (required for iOS)
+//     await notifee.requestPermission()
 
-async function onDisplayNotification() {
-    // Request permissions (required for iOS)
-    await notifee.requestPermission()
+//     // Create a channel (required for Android)
+//     const channelId = await notifee.createChannel({
+//       id: 'default',
+//       name: 'Default Channel',
+//     });
 
-    // Create a channel (required for Android)
-    const channelId = await notifee.createChannel({
-      id: 'default',
-      name: 'Default Channel',
-    });
-
-    // Display a notification
-    await notifee.displayNotification({
-      title: 'Notification Title',
-      body: 'Main body content of the notification',
-      android: {
-        channelId,
-        smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
-        // pressAction is needed if you want the notification to open the app when pressed
-        pressAction: {
-          id: 'default',
-        },
-      },
-    });
-  }
-// Import your action
+//     // Display a notification
+//     await notifee.displayNotification({
+//       title: 'Notification Title',
+//       body: 'Main body content of the notification',
+//       android: {
+//         channelId,
+//         smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+//         // pressAction is needed if you want the notification to open the app when pressed
+//         pressAction: {
+//           id: 'default',
+//         },
+//       },
+//     });
+//   }
+// // Import your action
 
 export async function notificationListeners(dispatch, newRequests, ongoingRequests, retailerHistory) {
     // const dispatch = useDispatch();
